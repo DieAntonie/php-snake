11f0c296a478cd98ccf123d7a794de24e250beb3
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2026-08-18 16:12:31 +0200

    Add info endpoint

---
+++ api/info.php
@@ -0,0 +1,10 @@
+<?php 
+    header('Content-Type: application/json');
+    return [
+        "apiVersion" => "1",
+        "author" => "Chris Antonie Pieterse",
+        "color" => "#ff00ff",
+        "headType" => "bendr",
+        "tailType" => "pixel"
+    ];
+?>
