5d8cc240c84fb505786bdda2196795cd754777cf
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2026-08-19 16:52:53 +0200

    Add API index

--- api/info.php
+++ api/info.php
@@ -1,4 +1,20 @@
-<?php 
+<?php
+/**
+ * GET /
+ * Battlesnake Details endpoint
+ * Called by the game engine to check connectivity and get snake details.
+ */
+
+// Handle GET request
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+    exit;
+}
+
+try {
+    // Return snake customization details
+    http_response_code(200);
     header('Content-Type: application/json');
     echo json_encode([
         "apiVersion" => "1",
@@ -7,4 +23,9 @@
         "headType" => "bendr",
         "tailType" => "pixel"
     ]);
+
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+}
 ?>
