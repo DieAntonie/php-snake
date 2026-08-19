0d4a96d146a491e537094b01df0eb11d76ad429c
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2026-08-19 17:54:05 +0200

    logger added

--- api/end.php
+++ api/end.php
@@ -1,6 +1,32 @@
 <?php
 
-require_once(__DIR__ . '/../index.php');
+
+/**
+ * Utility class for logging
+ */
+class Logger
+{
+    /**
+     * Write data to log file
+     * @param string $data Data to log
+     * @param string $file Log file path
+     */
+    public static function log(string $data, string $file = 'logs.txt'): void
+    {
+        file_put_contents($file, $data . PHP_EOL, FILE_APPEND | LOCK_EX);
+    }
+
+    /**
+     * Write JSON data to log file
+     * @param mixed $data Data to serialize
+     * @param string $file Log file path
+     */
+    public static function logJson($data, string $file = 'logs.json'): void
+    {
+        $json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        file_put_contents($file, $json . PHP_EOL . PHP_EOL, FILE_APPEND | LOCK_EX);
+    }
+}
 
 /**
  * POST /end
